458badb4e68f0fc47159ad03bf058d555ff30635
Ian Macali <ianmacalinao@gmail.com>
2011-12-04 22:22:11 -0600

    Flaggables update part 2.

--- src/main/java/com/crimsonrpg/personas/personasapi/npc/NPC.java
+++ src/main/java/com/crimsonrpg/personas/personasapi/npc/NPC.java
@@ -4,6 +4,7 @@
  */
 package com.crimsonrpg.personas.personasapi.npc;
 
+import com.crimsonrpg.flaggables.api.Flaggable;
 import com.crimsonrpg.personas.personasapi.persona.Persona;
 import java.util.List;
 import org.bukkit.Location;
@@ -12,44 +13,7 @@ import org.bukkit.entity.HumanEntity;
 /**
  * Represents an NPC.
  */
-public interface NPC {
-
-    /**
-     * Gets the ID of this NPC.
-     * 
-     * @return 
-     */
-    public String getId();
-
-    /**
-     * Gets the in-game display name of this NPC.
-     * 
-     * @return 
-     */
-    public String getName();
-
-    /**
-     * Gets a certain {@link Trait} of this NPC.
-     * 
-     * @param <T> The type of {@link Trait}.
-     * @param type The type of {@link Trait}.
-     * @return The {@link Trait}.
-     */
-    public <T extends Trait> T getTrait(Class<T> type);
-
-    /**
-     * Sets a {@link Trait} of this NPC.
-     * 
-     * @param trait The {@link Trait} to set.
-     */
-    public void setTrait(Trait trait);
-
-    /**
-     * Gets a list of all of an NPC's traits.
-     * 
-     * @return 
-     */
-    public List<Trait> getTraits();
+public interface NPC extends Flaggable {
 
     /**
      * Gets the {@link Persona} this NPC is affiliated with.
@@ -69,11 +33,6 @@ public interface NPC {
      * Despawns this NPC.
      */
     public void despawn();
-
-    /**
-     * Deletes this NPC from the server.
-     */
-    public void delete();
 
     /**
      * Gets the corresponding Bukkit handle of the NPC.
--- src/main/java/com/crimsonrpg/personas/personasapi/npc/NPCManager.java
+++ src/main/java/com/crimsonrpg/personas/personasapi/npc/NPCManager.java
@@ -4,69 +4,14 @@
  */
 package com.crimsonrpg.personas.personasapi.npc;
 
-import com.crimsonrpg.personas.personasapi.persona.Persona;
-import java.util.List;
 import org.bukkit.Location;
+
+import com.crimsonrpg.flaggables.api.FlaggableManager;
 
 /**
  * Represents a manager for {@link NPC}s.
  */
-public interface NPCManager {
-
-    /**
-     * Registers a {@link Trait} with the NPC manager.
-     * <p>NOTE: Register {@link Trait}s in the constructor of your
-     * plugin otherwise traits may not load correctly.
-     * 
-     * @param trait The type of the {@link Trait}.
-     */
-    public void registerTrait(Class<? extends Trait> type);
-
-    /**
-     * Gets a registered trait from its name.
-     * 
-     * @param name The name of the Trait.
-     * @return The Trait's type.
-     */
-    public Class<? extends Trait> getTraitType(String name);
-
-    /**
-     * Creates an {@link NPC} with the given parameters.
-     * 
-     * @param name The name of the {@link NPC} as shown in-game. (Must be 16 characters or less)
-     * @param traits The {@link Trait}s of the {@link NPC}.
-     * @param persona  The {@link Persona} that this {@link NPC} possesses.
-     * @return The {@link NPC} that was spawned.
-     */
-    public NPC createNPC(String name, List<Trait> traits, Persona persona);
-
-    /**
-     * Creates an {@link NPC} with the given parameters.
-     * 
-     * @param id The internal ID of the {@link NPC}.
-     *      If this ID is taken, then the {@link NPC} will not be spawned.
-     *      Instead, the old NPC with that ID will be returned.
-     * @param name The name of the {@link NPC} as shown in-game. (Must be 16 characters or less)
-     * @param traits The {@link Trait}s of the {@link NPC}.
-     * @param persona  The {@link Persona} that this {@link NPC} possesses.
-     * @return The NPC that was created or the original NPC.
-     */
-    public NPC createNPC(String id, String name, List<Trait> traits, Persona persona);
-
-    /**
-     * Deletes the {@link NPC} from the server permanently.
-     * 
-     * @param id The id of the {@link NPC} to delete.
-     * @return True if the deletion was successful.
-     */
-    public boolean deleteNPC(String id);
-
-    /**
-     * Deletes the given {@link NPC} from the server permanently.
-     * 
-     * @param npc The {@link NPC} to delete.
-     */
-    public void deleteNPC(NPC npc);
+public interface NPCManager extends FlaggableManager<NPC> {
 
     public void spawnNPC(String id, Location location);
 
@@ -75,20 +20,5 @@ public interface NPCManager {
     public void despawnNPC(String id);
 
     public void despawnNPC(NPC npc);
-
-    /**
-     * Gets an {@link NPC} from its ID.
-     * 
-     * @param id
-     * @return The {@link NPC} associated with the given id.
-     */
-    public NPC getNPC(String id);
-
-    /**
-     * Gets a list of all {@link NPC}s on the server.
-     * 
-     * @return A list of all {@link NPC}s.
-     */
-    public List<NPC> getNPCs();
 
 }
