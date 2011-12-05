5ed6299b223131f920762a95c479a3af1fb5184f
Ian Macali <ianmacalinao@gmail.com>
2011-12-04 23:27:33 -0600

    Human NPC inside of the API.

---
+++ src/main/java/com/crimsonrpg/personas/personasapi/npc/HumanNPC.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crimsonrpg.personas.personasapi.npc;
+
+import org.bukkit.entity.HumanEntity;
+
+/**
+ * Represents a human NPC.
+ */
+public interface HumanNPC extends NPC<HumanEntity> {
+}
