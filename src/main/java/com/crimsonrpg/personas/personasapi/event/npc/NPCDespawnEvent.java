06c04580f87fbb988620ff627fc6be0305fa0890
Ian Macali <ianmacalinao@gmail.com>
2011-12-09 22:33:17 -0600

    Fixed a bad event.

--- src/main/java/com/crimsonrpg/personas/personasapi/event/npc/NPCDespawnEvent.java
+++ src/main/java/com/crimsonrpg/personas/personasapi/event/npc/NPCDespawnEvent.java
@@ -6,25 +6,13 @@ package com.crimsonrpg.personas.personasapi.event.npc;
 
 import com.crimsonrpg.personas.personasapi.event.EventType;
 import com.crimsonrpg.personas.personasapi.npc.NPC;
-import org.bukkit.Location;
 
 /**
  * Called when an NPC is despawned.
  */
 public class NPCDespawnEvent extends NPCEvent {
-    private Location location;
 
-    public NPCDespawnEvent(NPC npc, Location location) {
+    public NPCDespawnEvent(NPC npc) {
         super(EventType.NPC_DESPAWN, npc);
-        this.location = location;
-    }
-    
-    /**
-     * Gets the location that the NPC was despawned at.
-     * 
-     * @return 
-     */
-    public Location getLocation() {
-        return location;
     }
 }
